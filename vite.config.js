4181cb819f9a0fb7598dc1b490462fa23cd8499f
Tymofii <tymofii.wga@gmail.com>
2024-02-15 18:52:38 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig(({ command }) => {
       sourcemap: true,
 
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: glob.sync('./src/*.html', './src/img/**.*'),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
